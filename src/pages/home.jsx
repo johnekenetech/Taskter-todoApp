6706d1714ad1c482b09aa096a2997988a49b8f29
John Ekene <johnekenetech@gmail.com>
2024-03-31 06:00:35 -0700

    second commit

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -9,7 +9,13 @@ const Home = () => {
        setName(e.target.value)
     }
     const handleNavigate = () => {
-        navigate('main', {state: {name}})
+        if(name == ''){
+            const nameInput = document.querySelector('#nameInput')
+            nameInput.placeholder = 'Enter name to proceed'
+        }else{
+            navigate('main', {state: {name}})
+        }
+       
     }
 
     return (
@@ -18,9 +24,9 @@ const Home = () => {
                     <h1 className="text-5xl mb-7 text-green-400 sm:text-7xl sm:mb-9 ">Tasker</h1>
                     <p className="text-white text-4xl leading-[60px] sm:text-5xl sm:leading-[75px]">Manage your <span className="text-green-400 border-b border-white-700"> Tasks </span><br/> with ease.</p>
                     
-                    <input type='text' placeholder="Enter Name" value={name} className=" font-semibold outline-none mt-8 pl-4 py-1 sm:pl-10 sm:py-2 md:px-10" onChange={handleChange} name={name}/>
+                    <input type='text' id="nameInput" placeholder="Enter name" value={name} className=" font-semibold outline-none mt-8 pl-4 py-1 sm:pl-10 sm:py-2 md:px-10" onChange={handleChange} name={name}/>
                     <div className="flex justify-center mt-12 sm:mt-16">
-                        <button onClick={handleNavigate}><img src="enter.png" alt="" className="sm:w-[75px]"/></button>
+                        <img src="src\enter.png" alt="" className="sm:w-[75px] cursor-pointer" onClick={handleNavigate}/>
                     </div>
                
             </div> 
